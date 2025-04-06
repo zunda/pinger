6ac9c5ba51960aab394aca4c4826dbae7eb18f18
zunda <zundan@gmail.com>
2025-04-05 14:36:02 -1000

    Add some tests for echo channel

--- test/channels/echo_channel_test.rb
+++ test/channels/echo_channel_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
 class EchoChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
+  test "subscribes" do
+    subscribe
+    assert subscription.confirmed?
+    assert_has_stream "echo"
+  end
 end
