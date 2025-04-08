75ea282b440808045dbb8839760b53cd40489d4a
zunda <zundan@gmail.com>
2025-04-07 22:16:52 -1000

    Expose target hostname from connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,9 +1,12 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :uuid
+    attr_reader :target
 
     def connect
       self.uuid = SecureRandom.uuid
+      req = self.request
+      @target = req.host
     end
   end
 end
--- test/channels/connection_test.rb
+++ test/channels/connection_test.rb
@@ -5,4 +5,10 @@ class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
     connect
     assert connection.uuid.match(/\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i)
   end
+
+  test "picks up target hostname" do
+    target = "pinger.example.com"
+    connect headers: { "Host" => target }
+    assert_equal target, connection.target
+  end
 end
